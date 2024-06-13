0fe32019042680abc367407a25124bf5ab7002b4
mikhail <6589665+mshavliuk@users.noreply.github.com>
2024-06-13 14:13:21 +0300

    improve pdf/a compliance

--- compile.sh
+++ compile.sh
@@ -5,9 +5,10 @@ if [ "$1" = "--fast" ]; then
     pdflatex -output-directory=compile "\\gdef\\draftmode{1} \\input{main.tex}" main.tex
     mv compile/main.pdf main.pdf
 else
-    latexmk -pdf -f main.tex
+    latexmk -pdf -shell-escape -f main.tex
     latexmk -c main.tex
     rm -f main.bbl main.glg main.glo main.gls main.ist main.upa main.upb main.xmpdata pdfa.xmpi
+    verapdf -f 3b --format text main.pdf
 fi
 
 open -a Preview main.pdf
